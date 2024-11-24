d6a1d33edb81c915d96b4cf60ed66b00d671f600
Endri-1 <endri.meta@city.ac.uk>
2024-11-24 16:47:40 +0000

    hw 5

--- HW 5/task1.sql
+++ HW 5/task1.sql
@@ -1,30 +1,30 @@
-1. 
+-- 1
 SELECT bill_date, bill_total
 FROM restBill
 WHERE cust_name = 'Bob Crow';
 
-2.
+-- 2
 SELECT DISTINCT cust_name
 FROM restBill
 WHERE cust_name LIKE '% Smith'
 ORDER BY cust_name DESC;
 
-3.
+-- 3
 SELECT cust_name
 FROM restBill
 WHERE cust_name LIKE '% C%';
 
-4.
+-- 4
 SELECT DISTINCT first_name, surname 
 FROM restStaff
 WHERE headwaiter IS NOT NULL;
 
-5.
+-- 5
 SELECT * 
 FROM restBill
 WHERE bill_date BETWEEN 160201 and 160229;
 
-6.
+-- 6
 SELECT bill_date
 FROM restBill
 WHERE bill_date BETWEEN 150101 and 151231
