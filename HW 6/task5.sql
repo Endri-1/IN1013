82f8ceb58e6deac3290c3346d4b0be5467a4dd30
Endri-1 <endri.meta@city.ac.uk>
2024-12-01 20:43:51 +0000

    HW

--- HW 6/task5.sql
+++ HW 6/task5.sql
@@ -1,5 +1,4 @@
 -- 1 
-
 SELECT first_name, surname, bill_date, COUNT(*) AS num_bills
 FROM restBill 
 JOIN restStaff  ON waiter_no = staff_no
@@ -7,21 +6,18 @@ GROUP BY first_name, surname, bill_date
 HAVING COUNT(*) >= 2;
 
 -- 2 
-
 SELECT room_name, COUNT(*) AS num_tables
 FROM restRest_table
 WHERE no_of_seats > 6
 GROUP BY room_name;
 
 -- 3 
-
 SELECT room_name, SUM(bill_total) AS total_bill_amount
 FROM restBill 
 JOIN restRest_table ON table_no = table_no
 GROUP BY room_name;
 
 -- 4 
-
 SELECT first_name AS headwaiter_first_name, surname AS headwaiter_surname, SUM(b.bill_total) AS total_bill_amount
 FROM restStaff 
 JOIN restStaff ON staff_no = headwaiter
@@ -30,14 +26,12 @@ GROUP BY first_name, surname
 ORDER BY total_bill_amount DESC;
 
 -- 5
-
 SELECT cust_name, AVG(bill_total) AS avg_spent
 FROM restBill
 GROUP BY cust_name
 HAVING AVG(bill_total) > 400;
 
 -- 6 
-
 SELECT s.first_name, s.surname, COUNT(*) AS num_bills
 FROM restBill b
 JOIN restStaff s ON b.waiter_no = s.staff_no
